c87c5c19c3d7b9abe44265a0980c98d8a6dd2194
Samuel Sobalvarro <dev.msobalvarro@gmail.com>
2025-01-01 09:33:30 -0600

    bug fixed

--- route/index.js
+++ route/index.js
@@ -3,34 +3,33 @@ import multer from 'multer'
 import path from 'path'
 import fs from 'fs'
 import { v4 as uuidv4 } from 'uuid'
-import { Router } from "express"
+import { Router } from 'express'
 import { fileURLToPath } from 'url'
 
 dotenv.config()
 
-// Define __dirname en ES Modules
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export const router = Router()
 
+const uploadDir = path.join(__dirname, process.env.PUBLIC_FOLDER)
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true })
+}
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '../uploads')
+    cb(null, uploadDir)
   },
   filename: (req, file, cb) => {
     const uniqueName = `${uuidv4()}${path.extname(file.originalname)}`
     cb(null, uniqueName)
-  }
+  },
 })
 
 const upload = multer({ storage })
-
-const uploadDir = path.join(__dirname, 'uploads')
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir)
-}
-
 
 router.post('/', upload.single('file'), (req, res) => {
   try {
@@ -38,10 +37,7 @@ router.post('/', upload.single('file'), (req, res) => {
       return res.status(400).send('No se ha subido ningún archivo.')
     }
 
-    res.status(200).send({
-      message: 'Archivo subido con éxito.',
-      fileName: req.file.filename,
-    })
+    res.status(200).send({ fileName: req.file.filename })
   } catch (error) {
     res.status(500).send({
       message: 'Ocurrió un error al subir el archivo.',
